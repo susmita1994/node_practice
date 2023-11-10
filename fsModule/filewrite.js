676495a6b40b4ff3a1a50389660798d71d04db84
susmita1994 <susmitadhua2012@gmail.com>
2023-11-10 18:09:00 +0530

    synchronous file write

--- fsModule/filewrite.js
+++ fsModule/filewrite.js
@@ -4,17 +4,29 @@ var server=http.createServer(function name(req,res) {
     
     if (req.url=='/') {
        //Asyn
-       fs.writeFile('demo.txt','Hello World',function (error) {
-            if(error){
-                res.writeHead(200,{'Content-Type':'text/html'});
-                res.write('file write failed');
-                res.end();
-            }else{
-                res.writeHead(200,{'Content-Type':'text/html'});
-                res.write('file write success');
-                res.end();
-            }
-       });
+    //    fs.writeFile('demo.txt','Hello World',function (error) {
+    //         if(error){
+    //             res.writeHead(200,{'Content-Type':'text/html'});
+    //             res.write('file write failed');
+    //             res.end();
+    //         }else{
+    //             res.writeHead(200,{'Content-Type':'text/html'});
+    //             res.write('file write success');
+    //             res.end();
+    //         }
+    //    });
+
+       //Sync
+       let error=fs.writeFileSync('demo1.text','this is Synchronous file');
+       if(error){
+            res.writeHead(200,{'Content-Type':'text/html'});
+            res.write('file write failed');
+            res.end();
+        }else{
+            res.writeHead(200,{'Content-Type':'text/html'});
+            res.write('file write success');
+            res.end();
+        }
     }
 });
 server.listen(5050);
