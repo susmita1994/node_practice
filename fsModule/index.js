10e1ec014ede249bb65fffa7f00efd0e7e6efc79
susmita1994 <susmitadhua2012@gmail.com>
2023-11-10 17:18:52 +0530

    synchronous file system

--- fsModule/index.js
+++ fsModule/index.js
@@ -2,12 +2,19 @@ var fs=require('fs');
 var http=require('http');
 var server=http.createServer(function name(req,res) {
     if (req.url=='/') {
-        fs.readFile("home.html",function(error,data) {
-            res.writeHead(200,{'Content-Type':'text/html'});
-            res.write(data);
-            res.end();
-        });
+        //Asyn 
+        // fs.readFile("home.html",function(error,data) {
+        //     res.writeHead(200,{'Content-Type':'text/html'});
+        //     res.write(data);
+        //     res.end();
+        // });
+
+        //sync
+       let mydata=fs.readFileSync('home.html');
+        res.writeHead(200,{'Content-Type':'text/html'});
+        res.write(mydata);
+        res.end();
     }
 });
-server.listen(5050);
+server.listen(4040);
 console.log('server run successfully');
