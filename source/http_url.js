3e79feb1141e49d3617b95ed1a34f1a2430e0322
susmita1994 <susmitadhua2012@gmail.com>
2023-11-10 15:40:27 +0530

    url module split url

---
+++ source/http_url.js
@@ -0,0 +1,15 @@
+var http=require('http');
+var url=require('url');
+var server=http.createServer(function (req,res) {
+   var myUrl="https://cygnusweb.in/atdc_new/publication?faculty=&year=2023&DataTable_length=10";
+   var myURLObj=url.parse(myUrl,true); //split the url
+   var myHostName=myURLObj.host;
+    var myPathName=myURLObj.pathname;
+    var mySearchName=myURLObj.search;
+
+    res.writeHead(200,{'Content-Type':'text/html'});
+    res.write(myHostName);
+    res.end();
+});
+server.listen(5000);
+console.log('server created successfully');
