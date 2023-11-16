8246fc2751de3e02cf29dc9fffb805d4f3cf47b5
susmita1994 <susmitadhua2012@gmail.com>
2023-11-16 17:47:52 +0530

    Express routing

--- express_js/practiceApp1/index.js
+++ express_js/practiceApp1/index.js
@@ -3,7 +3,15 @@ var express=require('express');
 var app=express();
 
 app.get('/',function (req,res) {
-    res.send('<h1>My first Express Application</h1>');
+    res.send('<h1>Home Page</h1>');
+});
+
+app.get('/about',function (req,res) {
+    res.send('<h1>About Page</h1>');
+});
+
+app.get('/contact',function (req,res) {
+    res.send('<h1>Contact Page</h1>');
 });
 
 app.listen(8080,function () {
